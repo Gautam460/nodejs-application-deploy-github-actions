7b16c305da352bec19d9a9842fb5afea745c420c
Gautam460 <gautamsingh186939@gmail.com>
2026-05-11 12:42:39 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the e-commerce web server!');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
